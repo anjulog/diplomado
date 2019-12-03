c865708d22c049b039e3b7d5c8f1939f016d9ae0
anjulog <38738078+anjulog@users.noreply.github.com>
2019-12-03 16:32:22 -0500

    Add files via upload

---
+++ config-service/src/main/java/com/uniajc/diplomado/services/config/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.uniajc.diplomado.services.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication
+{
+
+    public static void main (String[] args)
+    {
+        new SpringApplicationBuilder (ConfigApplication.class).run (args);
+    }
+
+}
